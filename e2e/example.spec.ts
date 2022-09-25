eebe0bda94cd46b37a3801e3b8425b9202ab9344
Woo Sunguk <woosunguk@jiran.com>
2022-09-25 21:32:07 +0900

    TailwindCSS, MUI, MUI Joy  설치

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,9 +4,5 @@ test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // Find an element with the text 'About Page' and click on it
-  await page.click('text=About Page')
-  // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('/about')
-  // The new page should contain an h1 with "About Page"
-  await expect(page.locator('h1')).toContainText('About Page')
+  await expect(page.locator('h1')).toContainText('요리 레시피 사이트')
 })
---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { CssVarsProvider, StyledEngineProvider } from "@mui/joy";
+
+import customTheme from "../theme";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <React.StrictMode>
+        {/* <StyledEngineProvider injectFirst>
+          <CssVarsProvider disableTransitionOnChange theme={customTheme}> */}
+        <Component {...pageProps} />
+        {/* </CssVarsProvider>
+        </StyledEngineProvider> */}
+      </React.StrictMode>
+    </>
+  );
+}
+
+export default MyApp;
